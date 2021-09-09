aa8049b23125fa1bd9847f9d492e7ae3e225b46d
Samantha Puterman <Samanpg@bu.edu>
2021-09-09 16:16:25 -0400

    barcode

--- App.js
+++ App.js
@@ -1,26 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-
-//Imports for Expo Camera
-import { useState, useEffect } from 'react';
-import { TouchableOpacity } from 'react-native';
-import { Camera } from 'expo-camera'; //should unmount if we have multiple screens
-
-//Barcode Scanner
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
+
 
 
 export default function App() {
   const [hasPermission, setHasPermission] = useState(null);
-  const [type, setType] = useState(Camera.Constants.Type.back);
-
   const [scanned, setScanned] = useState(false);
-
 
   useEffect(() => {
     (async () => {
-      //const { status } = await Camera.requestPermissionsAsync();
       const { status } = await BarCodeScanner.requestPermissionsAsync();
       setHasPermission(status === 'granted');
     })();
@@ -30,58 +19,29 @@ export default function App() {
     setScanned(true);
     alert(`Bar code with type ${type} and data ${data} has been scanned!`);
   };
-  
-  if (hasPermission === null) {
-    return <View />;
-  }
 
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
   if (hasPermission === false) {
     return <Text>No access to camera</Text>;
   }
 
-
   return (
     <View style={styles.container}>
-      <Camera style={styles.camera} type={type}>
-        <View style={styles.buttonContainer}>
-          <BarCodeScanner
-            onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-            style={StyleSheet.absoluteFillObject}
-          />
-          {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
-          
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => {
-              setType(
-                type === Camera.Constants.Type.back
-                  ? Camera.Constants.Type.front
-                  : Camera.Constants.Type.back
-              );
-            }}>
-            <Text style={styles.text}> Flip </Text>
-          </TouchableOpacity>
-        </View>
-      </Camera>
-      {/* <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" /> */}
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />
+      {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
     </View>
   );
 }
 
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    //alignItems: 'center',
+    flexDirection: 'column',
     justifyContent: 'center',
   },
-
-  camera: {
-    flex: 0.9,
-    //backgroundColor: '#fff',
-    //alignItems: 'center',
-    //justifyContent: 'center',
-  }
 });
