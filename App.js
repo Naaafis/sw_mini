15a5ff254385f37297f19cd4dde73b1193fff71b
Samantha Puterman <Samanpg@bu.edu>
2021-09-11 21:13:00 -0400

    API logic

--- App.js
+++ App.js
@@ -1,47 +1,99 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'; //use effect does something after render 
 import { Text, View, StyleSheet, Button } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-
-
-export default function App() {
+function App() {
+  var prod_id = 0; 
+  //const [prod_id, setId, getId] = useState(0); 
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
+  const [isLoading, setLoading] = useState(true);
+
+  const getDataFromApi = async (prod_id) => {
+    try {
+      console.log('getdata'); 
+    let response = await  fetch('https://api.nal.usda.gov/fdc/v1/foods/search?api_key=XQ1wRrZ2wrECEgeGRfbyisWLBOs52e3ClNdZAFZC', {
+  method: 'POST',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({
+    query: prod_id //{'012000171758'}
+  })
+}); 
+  let json = await response.json();  
+  //console.log('here and prod_id is');
+ // console.log({prod_id});  
+  console.log(JSON.stringify(json.foods[0].description));
+ // return json.foods; 
+  //console.lJSON.parse(json.food); 
+} catch (error) {
+  console.log('papappapppapp')
+  console.error(error);
+}
+};
 
   useEffect(() => {
-    (async () => {
-      const { status } = await BarCodeScanner.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
-    })();
-  }, []);
+     (async () => {
+       const { status } = await BarCodeScanner.requestPermissionsAsync();
+       setHasPermission(status === 'granted');
+     })();
+   }, []);
 
   const handleBarCodeScanned = ({ type, data }) => {
-    setScanned(true);
-    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
-  };
+     setScanned(true);
+   //  alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+     //processing of data depending on barcode type (i.e removing a zero or not))
+     console.log(type); 
+     if (type=='org.gs1.EAN-13')
+     {
 
-  if (hasPermission === null) {
-    return <Text>Requesting for camera permission</Text>;
-  }
-  if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
-  }
+      console.log('inside'); 
+      prod_id = parseInt(data, 10); 
+     // setId(parseInt(data, 10)); 
+     // console.log(await getId());  
+     }
+     else 
+     {
+      console.log('outside'); 
+      prod_id = data; 
+     // setId(data); 
+     }
+     console.log(prod_id); 
+     getDataFromApi(prod_id); 
+   };
+
+
+   if (hasPermission === null) {
+     return <Text>Requesting for camera permission</Text>;
+   }
+   if (hasPermission === false) {
+     return <Text>No access to camera</Text>;
+   }
+
 
   return (
-    <View style={styles.container}>
+    <View style={styles.container}> 
+      <Text> Hello </Text> 
       <BarCodeScanner
-        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-        style={StyleSheet.absoluteFillObject}
-      />
-      {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
-    </View>
-  );
+         onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+         style={StyleSheet.absoluteFillObject}
+       />
+       {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
+    </View> 
+    ); 
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-  },
-});
+   container: {
+     flex: 1,
+     flexDirection: 'column',
+     alignItems: 'center',
+     justifyContent: 'center',
+     },
+ });
+
+export default App; 
